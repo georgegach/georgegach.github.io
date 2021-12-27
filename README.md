ab713d9f1c8eada9ef153dbb4592a2b79852d010
George Gach <georgegach@outlook.com>
2021-12-27 13:14:28 +0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# George Gach's website with Next.js
+
+Check out latest build at [next.george.anbani.ge](https://next.george.anbani.ge)
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+const colors = require('tailwindcss/colors')
+
+
+module.exports = {
+  purge: ['./components/**/*.js', './pages/**/*.js'],
+
+  variants: {
+    // The 'active' variant will be generated in addition to the defaults
+    extend: {
+    }
+  },
+
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          ...colors.indigo
+        },
+
+        secondary: {
+          ...colors.slate
+        },
+
+        
+      },
+
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        '18': 'repeat(18, minmax(0, 1fr))',
+        '36': 'repeat(36, minmax(0, 1fr))',
+      }, 
+      
+      fontFamily: {
+        sans: ['Anbani Noto Sans', 'sans-serif'],
+        serif: ['Anbani Noto Serif', 'serif'],
+      },
+
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
+}
